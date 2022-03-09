aba25190d28275242229a9b1952cf58abad1f5db
Alzound <61034726+Alzound@users.noreply.github.com>
2022-03-09 17:34:27 -0600

    blur

--- Model.java
+++ Model.java
@@ -151,6 +151,8 @@ public class Model {
             }
         }*/
         //blur
+        /*//double [][] color = new double[3][3];
+        double [][] color = {{0,.2,0}, {.2,.2,.2}, {0,.2,0}};
         for (int i =0; i < w-1; i++)
         {
             for (int j =0; j < h-1; j++)
@@ -177,7 +179,54 @@ public class Model {
                 //System.out.println(bandw); 
             }
         }
-        
+        */
+        int i = 0;
+        int max = 400, rad = 10;
+        int a1 = 0, r1 = 0, g1 = 0, b1 = 0;
+        Color color[];
+        color = new Color[max];
+ 
+ 
+        int x = 1, y = 1, x1, y1, ex = 5, d = 0;
+ 
+ 
+        for (x = rad; x < imagen.getHeight() - rad; x++) {
+            for (y = rad; y < imagen.getWidth() - rad; y++) {
+                for (x1 = x - rad; x1 < x + rad; x1++) {
+                    for (y1 = y - rad; y1 < y + rad; y1++) {
+                        color[i++] = new Color(
+                            imagen.getRGB(y1, x1));
+                    }
+                }
+ 
+                // Smoothing colors of image
+                i = 0;
+                for (d = 0; d < max; d++) {
+                    a1 = a1 + color[d].getAlpha();
+                }
+ 
+                a1 = a1 / (max);
+                for (d = 0; d < max; d++) {
+                    r1 = r1 + color[d].getRed();
+                }
+ 
+                r1 = r1 / (max);
+                for (d = 0; d < max; d++) {
+                    g1 = g1 + color[d].getGreen();
+                }
+ 
+                g1 = g1 / (max);
+                for (d = 0; d < max; d++) {
+                    b1 = b1 + color[d].getBlue();
+                }
+ 
+                b1 = b1 / (max);
+                int sum1 = (a1 << 24) + (r1 << 16)
+                           + (g1 << 8) + b1;
+                newImage.setRGB(y, x, (int)(sum1));
+            }
+        }
+ 
         m.writeImage(imagen, "starwars_blur", "jpg");
        
         //System.out.println("mult"+ red);
